9ed479dd1d91df26641bb8eaa4cfe05cb95e7e67
shreya679 <82694864+shreya679@users.noreply.github.com>
2023-12-18 10:23:30 +0530

    Add files via upload

---
+++ loginform/script.js
@@ -0,0 +1,12 @@
+const title=document.getElementById('title');
+const namefield=document.getElementById('namefield');
+const signinBtn=document.getElementById('signinBtn');
+const signupBtn=document.getElementById('signupBtn');
+signupBtn.onclick=function(){
+    title.innerHTML="sign up";
+    namefield.style.display="block";
+}
+signinBtn.onclick=function(){
+    title.innerHTML="Login";
+    namefield.style.display="block";
+}
